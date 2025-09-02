697b01d291efd4312aedb248b188f6f80e796c52
Dipika Sonkusre <dipika@weetechsolution.com>
2025-09-02 15:01:03 +0530

    feat: React UseEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import UseEffect from "./Hooks/UseEffect";
 import UseState from "./Hooks/UseState";
 
 function App() {
   return (
     <section>
-      <UseState />
+      <UseState /> <br />
+      <UseEffect />
     </section>
   );
 }
