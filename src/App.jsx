c6bd1252194c77a4f72e89b2ed06dec1a6badb28
Dipika Sonkusre <dipika@weetechsolution.com>
2025-09-03 17:40:52 +0530

    feat: React UseContext Hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
 import "./App.css";
+import Footer from "./component/Footer";
+import Profile from "./component/Profile";
+import ContextProvider from "./context/AppContext";
 import UseCallback from "./Hooks/UseCallback";
 
 function App() {
@@ -8,7 +11,20 @@ function App() {
       {/* <UseEffect /> <br /> */}
       {/* <UseRef /> <br /> */}
       {/* <UseMemo /> <br /> */}
-      <UseCallback />
+      {/* <UseCallback /> */}
+
+      <div
+        style={{
+          border: "1px solid black",
+          padding: "1rem",
+          width: "500px",
+        }}
+      >
+        <ContextProvider>
+          <Profile />
+          <Footer />
+        </ContextProvider>
+      </div>
     </section>
   );
 }
